75acde9c7dac7a2a8f49c49af178811ec5f62fcf
BrianCottrell <Brian.Cottrell.mobile@gmail.com>
2017-06-24 17:14:35 -0700

    Initial commit

---
+++ Assets/Scripts/CarpCommands.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class CarpCommands : MonoBehaviour
+{
+    Vector3 originalPosition;
+    public Transform[] target;
+    public float speed;
+    private int current;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Grab the original local position of the sphere when the app starts.
+        originalPosition = this.transform.localPosition;
+    }
+
+    private void Update()
+    {
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+            Vector3 targetDir = target[current].position - transform.position;
+            float step = speed * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
+        else
+        {
+            current = (current + 1) % target.Length;
+        }
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+
+    // Called by SpeechManager when the user says the "Reset world" command
+    void OnReset()
+    {
+        // If the sphere has a Rigidbody component, remove it to disable physics.
+        var rigidbody = this.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            DestroyImmediate(rigidbody);
+        }
+
+        // Put the sphere back into its original local position.
+        this.transform.localPosition = originalPosition;
+    }
+
+    // Called by SpeechManager when the user says the "Drop sphere" command
+    void OnDrop()
+    {
+        // Just do the same logic as a Select gesture.
+        OnSelect();
+    }
+
+    void OnNoGravity()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    void OnHighGravity()
+    {
+        var rigidbody = this.gameObject.GetComponent<Rigidbody>();
+        rigidbody.useGravity = true;
+    }
+
+    void OnUnderwater()
+    {
+        var rigidbody = this.gameObject.GetComponent<Rigidbody>();
+        rigidbody.useGravity = true;
+    }
+}
---
+++ hololens1/Assets/Fall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour {
+	Rigidbody rigidbody;
+
+	void Start() {
+		rigidbody = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	public void toggleFalling() {
+		rigidbody.useGravity = !rigidbody.useGravity;
+	}
+}
