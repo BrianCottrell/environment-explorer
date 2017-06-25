ccb4a27e580e1f3aff44a4040d710a6d436b1fea
Thomas Suarez <code@tomthecarrot.com>
2017-06-25 11:46:10 -0700

    Add sardine voice commands

---
+++ Assets/Resources/Sardine/Scripts/SardineCommands.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SardineCommands : MonoBehaviour {
+	public GameObject sardine;
+	public GameObject sardines;
+
+	public void Assemble() {
+		sardines.GetComponent<SardineBoidsController>().enabled = true;
+	}
+
+	public void Disperse() {
+		sardines.GetComponent<SardineBoidsController>().enabled = false;
+	}
+
+	public void Go() {
+		sardine.GetComponent<SardineCharacter>().forwardSpeed = 1;
+	}
+
+	public void Stop() {
+		sardine.GetComponent<SardineCharacter>().forwardSpeed = 0;
+	}
+}
